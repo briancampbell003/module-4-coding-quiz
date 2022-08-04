d95564ff2bcf92608849d3664a77e52252651fc8
Brian Campbell <brian.campbell003@gmail.com>
2022-08-04 00:23:13 -0500

    Landing page, added startBtn w/ working timer

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,24 @@
-function
-cool
+
+// START BUTTON + TIMER
+var startBtn = document.getElementById("startBtn");
+
+var timeEl = document.getElementById("timediv");
+var secondsLeft = 60;
+
+startBtn.addEventListener("click", function startTimer() {
+        var timerInterval = setInterval(function() {
+        secondsLeft--;
+        timeEl.innerHTML = secondsLeft + " seconds left";
+            
+        if(secondsLeft <= 10) {
+            timeEl.setAttribute("style", "color: darkred; font-size: 20px; font-weight: bold");
+        }
+
+        // if(secondsLeft === 0) {
+        //   clearInterval(timerInterval);
+        //   // Calls game over function
+        //   gameOver();
+        // }
+    
+        }, 1000);
+    });
