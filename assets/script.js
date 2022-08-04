120afe4429a022f382f69cd0346bdf9c26ccec3b
Brian Campbell <brian.campbell003@gmail.com>
2022-08-04 01:02:05 -0500

    Started JS question sequence

--- assets/script.js
+++ assets/script.js
@@ -5,20 +5,24 @@ var startBtn = document.getElementById("startBtn");
 var timeEl = document.getElementById("timediv");
 var secondsLeft = 60;
 
-startBtn.addEventListener("click", function startTimer() {
-        var timerInterval = setInterval(function() {
-        secondsLeft--;
-        timeEl.innerHTML = secondsLeft + " seconds left";
-            
-        if(secondsLeft <= 10) {
-            timeEl.setAttribute("style", "color: darkred; font-size: 20px; font-weight: bold");
-        }
+startBtn.addEventListener("click", function startQuiz() {
+    document.querySelector(".landing").hidden = true;
 
-        // if(secondsLeft === 0) {
-        //   clearInterval(timerInterval);
-        //   // Calls game over function
-        //   gameOver();
-        // }
-    
-        }, 1000);
-    });
+    var question1 = document.querySelector(".question");
+    question1.textContent = "Where's my car?";
+
+    var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.innerHTML = secondsLeft + " seconds left";
+        
+    if(secondsLeft <= 10) {
+        timeEl.setAttribute("style", "color: darkred; font-size: 20px; font-weight: bold");
+    }
+
+    // if(secondsLeft === 0) {
+    //   clearInterval(timerInterval);
+    //   // Calls game over function
+    //   gameOver();
+    // }
+    }, 1000);
+});
