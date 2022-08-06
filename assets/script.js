d00f20e75d6c557d6c59892348923d50f9ee6679
Brian Campbell <brian.campbell003@gmail.com>
2022-08-06 01:00:33 -0500

    Completed questions, tried to fix right/wrong issues

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,25 @@
-const qArray = ["Javascript is to an html document using which tag?",
+const qArray = [
+    "Javascript is called in an html document using which tag?",
     "In the context of web development, what does DOM stand for?",
     "Which keyword will declare a variable in Javascript?",
     "Which item below is a Boolean data type?",
     "Which Javascript method can be used to change display font?",
-    "etc"];
+    "What does the triple equal sign (===) indicate in Javascript?",
+    "Select the choice with correct syntax for opening a Javascript 'if' statement?",
+    "What extension do Javascript files use?"
+];
+
 const choiceArray = [
     q0arr = ["<style>", "<link>", "<script>", "<cookies>"],
     q1arr = ["Delicious Organic Meals", "Document Object Model", "Data Orientation Map", "Document Organization Marker"],
     q2arr = ["'let'", "'define'", "'function'", "'JSON'"],
     q3arr = ["Happy Friday!", "4893.35", "true", "NaN"],
     q4arr = ["setAttribute", "changeFont", "createElement", "textContent"],
-    q5arr = ["Chips", "Candy", "Yum yum yuafdsgm", "so good!"],
-    q6arr = ["Chipasdfgs", "Candy", "Yum yum yum", "so good!"],
-    q7arr = ["Chips", "Candy", "Yum yumasfdg yum", "so good!"],
-    q8arr = ["Chips", "Candy", "Yum yum yum", "so good!"],
-    q9arr = ["Chips", "Candy", "Yum yum yum", "so good!"],
+    q5arr = ["Equal values", "Parse element", "Strict equality (type and value)", "True"],
+    q6arr = ["if x, then {", "if {x},", "if (x) {", "if 'x' {"],
+    q7arr = [".html", ".js", ".css", ".java"]
 ]
-const rightAnswers = [ choiceArray[0][2], choiceArray[1][1], choiceArray[2][0], choiceArray[3][3] ];
+const rightAnswers = [ choiceArray[0][2], choiceArray[1][1], choiceArray[2][0], choiceArray[3][2], choiceArray[4][0], choiceArray[5][2], choiceArray[6][2], choiceArray[7][1] ];
 // HOPEFULLY NOT NECESSARY -> const wrongAnswers = [];
 
 
@@ -25,10 +28,10 @@ let startBtn = document.getElementById("startBtn");
 let timeEl = document.getElementById("timediv");
 let question = document.querySelector(".question");
 let listEl = document.querySelector(".choices");
+let btn0 = document.getElementById("btn0");
 let btn1 = document.getElementById("btn1");
 let btn2 = document.getElementById("btn2");
 let btn3 = document.getElementById("btn3");
-let btn4 = document.getElementById("btn4");
 let liveCheck = document.getElementById("liveCheck");
 let secondsLeft = 60;
 let i = 0;  
@@ -47,77 +50,72 @@ startBtn.addEventListener("click", function startQuiz() {
 // Generate questions
 function generateQuestion() {
     document.querySelector(".qPages").hidden = false;
-   
+
     question.textContent = qArray[i];
 
 // Generate listed answer buttons using const arrays
 
-    btn1.textContent = choiceArray[i][0];
-    btn2.textContent = choiceArray[i][1];
-    btn3.textContent = choiceArray[i][2];
-    btn4.textContent = choiceArray[i][3];
+    btn0.textContent = choiceArray[i][0];
+    btn1.textContent = choiceArray[i][1];
+    btn2.textContent = choiceArray[i][2];
+    btn3.textContent = choiceArray[i][3];
         
 // Add listeners for next function
-    btn1.addEventListener("click", Next);
-    btn2.addEventListener("click", Next);
-    btn3.addEventListener("click", Next);
-    btn4.addEventListener("click", Next);
+    // btn1.addEventListener("click", Next);
+    // btn2.addEventListener("click", Next);
+    // btn3.addEventListener("click", Next);
+    // btn4.addEventListener("click", Next);
 
 // Add right/wrong answer listeners
+    if (rightAnswers.includes(btn0.textContent)) {
+        btn0.addEventListener("click", rightAns);
+    } else{
+        btn0.addEventListener("click", wrongAns);
+    }
+
     if( rightAnswers.includes(btn1.textContent)) {
         btn1.addEventListener("click", rightAns);
-    }
-    else{
+    } else{
         btn1.addEventListener("click", wrongAns);
     }
-    if( rightAnswers.includes(btn1.textContent)) {
+
+    if( rightAnswers.includes(btn2.textContent)) {
+
         btn2.addEventListener("click", rightAns);
-    }
-    else{
+    } else{
+        console.log(btn2.textContent);
         btn2.addEventListener("click", wrongAns);
     }
-    if( rightAnswers.includes(btn1.textContent)) {
+
+    if( rightAnswers.includes(btn3.textContent)) {
         btn3.addEventListener("click", rightAns);
-    }
+    } 
     else{
         btn3.addEventListener("click", wrongAns);
     }
-    if( rightAnswers.includes(btn1.textContent)) {
-        btn4.addEventListener("click", rightAns);
-    }
-    else{
-        btn4.addEventListener("click", wrongAns);
-    }
 }
 
-function Next(){
-    i++;
-    generateQuestion();
-    // event.stopPropagation;
-}
+// function Next(){
+//     i++;
+//     generateQuestion();
+//     // event.stopPropagation;
+// }
 
 function rightAns(){
-    liveCheck.textContent = "Correct!";
+    liveCheck.textContent = "Correct! ✅ 😀";
+    console.log("yes!");
+    i++;
+    generateQuestion();
 }
 
 function wrongAns(){
-    liveCheck.textContent = "Wrong!";
+    liveCheck.textContent = "Wrong! ❌ 😢";
+    // secondsLeft = secondsLeft - 10;
+    console.log("nope!!");
+    i++;
+    generateQuestion();
 }
 
-
-// ADD RIGHT/WRONG ANSWER LOGIC
-
-
-
-
-    // buttonEl.addEventListener("click", function(){
-    //     i++;
-    //     generateQuestions();
-    //     })
-
-
-
-console.log(i);
 
 function startTimer() {
     let timerInterval = setInterval(function() {
